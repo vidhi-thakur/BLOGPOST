1eabd8a7f2aaa8384926f1a1a24391460f13fc15
vidhi-thakur <72219676+vidhi-thakur@users.noreply.github.com>
2021-06-05 16:29:55 +0530

    Navbar
    added Navbar file to the project for navigation bar

--- blog-app/src/App.js
+++ blog-app/src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import "./components/Navbar";
+import React from 'react';
 
 function App() {
   return (
     <div className="App">
-      <h1>hi.</h1>
+      
     </div>
   );
 }
---
+++ blog-app/src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+
+function Navbar() {
+    return (
+        <>
+            Router
+        
+        </>
+    );
+}
+
+export default Navbar;
