3a59ea1bf17fb52f88de574fd2593db83493bfbd
vidhi-thakur <72219676+vidhi-thakur@users.noreply.github.com>
2021-06-05 18:01:46 +0530

    style: navbar

--- blog-app/src/App.js
+++ blog-app/src/App.js
@@ -1,12 +1,30 @@
 import './App.css';
-import "./components/Navbar";
+import Navbar from "./components/Navbar.js";
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <>
+            <Router>
+                <Navbar />
+                <Switch>
+                    <Route exact path="/">
+                        Home
+                    </Route>
+                    <Route path="/categouries">
+                        Categouries
+                    </Route>
+                    <Route path="/notifications">
+                        Notification
+                    </Route>
+                    <Route path="/account">
+                        Account
+                    </Route>
+                </Switch>
+            </Router>
+        
+        </>
   );
 }
 
--- blog-app/src/components/Navbar.js
+++ blog-app/src/components/Navbar.js
@@ -1,12 +1,25 @@
 import React from 'react'
-import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 function Navbar() {
     return (
-        <>
-            Router
-        
-        </>
+        <div>
+            <Link to="/" className="BlogPost">Blog Post.</Link>
+            <ul>
+                <li class="nav-links">
+                    <Link to="/"><i className="fas fa-home"></i></Link>
+                </li>
+                <li class="nav-links">
+                    <Link to="/categouries/"><i className="fas fa-compass"></i></Link>
+                </li>
+                <li class="nav-links">
+                    <Link to="/notifications/"><i className="fas fa-bell"></i></Link>
+                </li>
+                <li class="nav-links">
+                    <Link to="/account/"><i className="fas fa-user"></i></Link>
+                </li>
+            </ul>
+        </div>
     );
 }
 
