cb40f8bf6f79c9af254a203118ef80bba1ce7462
vidhi-thakur <72219676+vidhi-thakur@users.noreply.github.com>
2021-06-06 00:34:27 +0530

    style

--- blog-app/src/components/Navbar.js
+++ blog-app/src/components/Navbar.js
@@ -1,23 +1,32 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { Link } from "react-router-dom";
+import "./Navbar.css"
 
 function Navbar() {
+    const [click, setClick] = useState(false);
+    const onClickHandler = () => setClick(!click);
+
     return (
-        <header className="navbar"> 
-            <div>
+        <header className="nav-header"> 
+            <div className="heading-comtainer">
                 <Link to="/" className="nav-heading">Blog Post.</Link>
             </div>
-            <ul>
-                <li class="nav-links">
+                {/* <i className={click ? "fas fa-times" : "fas fa-bar"}></i> */}
+            {/* <div class="">
+                <Link to="/" className="nav-heading"></Link>
+            </div> */}
+
+            <ul className="nav-icons">
+                <li className="nav-links">
                     <Link to="/"><i className="fas fa-home"></i></Link>
                 </li>
-                <li class="nav-links">
+                <li className="nav-links">
                     <Link to="/categouries/"><i className="fas fa-compass"></i></Link>
                 </li>
-                <li class="nav-links">
+                <li className="nav-links">
                     <Link to="/notifications/"><i className="fas fa-bell"></i></Link>
                 </li>
-                <li class="nav-links">
+                <li className="nav-links">
                     <Link to="/account/"><i className="fas fa-user"></i></Link>
                 </li>
             </ul>
