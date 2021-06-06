4955eacbd6a556b14465aa4030f4ab49d7620933
vidhi-thakur <72219676+vidhi-thakur@users.noreply.github.com>
2021-06-06 16:50:30 +0530

    Home page added

---
+++ blog-app/src/components/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Link } from "react-router-dom";
+import "./Home.css"
+
+function Home() {
+    return (
+        <div className="Home-body">
+            
+        </div>
+    );
+}
+
+export default Home;
