168e6b81b9fe1c87165e9d5352b4f3d96599a477
vidhi-thakur <72219676+vidhi-thakur@users.noreply.github.com>
2021-06-07 17:29:14 +0530

    Updated Navbar.css

--- blog-app/src/components/Navbar.js
+++ blog-app/src/components/Navbar.js
@@ -1,6 +1,11 @@
 import React, {useState} from 'react'
 import { Link } from "react-router-dom";
 import "./Navbar.css"
+import HomeIcon from '@material-ui/icons/Home';
+import CategoryIcon from '@material-ui/icons/Category';
+import NotificationsNoneIcon from '@material-ui/icons/NotificationsNone';
+import AccountCircleIcon from '@material-ui/icons/AccountCircle';
+import SearchIcon from '@material-ui/icons/Search';
 
 function Navbar() {
     const [click, setClick] = useState(false);
@@ -9,26 +14,28 @@ function Navbar() {
     return (
         <header className="header"> 
             <div className="header__left">
-                <Link to="/" className="nav-heading">Blog Post.</Link>
+                <Link to="/" className="header__heading">Blog Post.</Link>
+                <div className="header__searchfield">
+                    <SearchIcon className="header__searchIcon" />
+                    <input className="header__input" type="text" />
+                </div>
             </div>
             <div class="header__right">
-                <ul className="header">
-                    <li className="nav-links">
-                        <Link to="/"><i className="fas fa-home"></i></Link>
+                <ul className="header__icontable">
+                    <li className="header__lists">
+                        <Link to="/"><HomeIcon className="header__icons" /></Link>
                     </li>
-                 <li className="nav-links">
-                        <Link to="/categouries/"><i className="fas fa-compass"></i></Link>
+                 <li className="header__lists">
+                        <Link to="/categouries/"><CategoryIcon className="header__icons" /></Link>
                  </li>
-                    <li className="nav-links">
-                        <Link to="/notifications/"><i className="fas fa-bell"></i></Link>
+                    <li className="header__lists">
+                        <Link to="/notifications/"><NotificationsNoneIcon className="header__icons" /></Link>
                     </li>
-                    <li className="nav-links">
-                        <Link to="/account/"><i className="fas fa-user"></i></Link>
+                    <li className="header__lists">
+                        <Link to="/account/"><AccountCircleIcon className="header__icons" /></Link>
                     </li>
                 </ul>
             </div>
-
-            
         </header>
     );
 }
