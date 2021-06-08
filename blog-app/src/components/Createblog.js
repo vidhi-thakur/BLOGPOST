708a52ee6d7c66dc942682dcc58f2bf74931b6da
vidhi-thakur <72219676+vidhi-thakur@users.noreply.github.com>
2021-06-08 19:17:55 +0530

    style: createblog.css
    styled createblog page

--- blog-app/src/components/Createblog.js
+++ blog-app/src/components/Createblog.js
@@ -1,20 +1,34 @@
 import React from 'react';
-import "./Createblog.css"
+import "./Createblog.css";
+import ClearRoundedIcon from '@material-ui/icons/ClearRounded';
+import AccountCircleIcon from '@material-ui/icons/AccountCircle';
+import FontDownloadRoundedIcon from '@material-ui/icons/FontDownloadRounded';
 
 function Createblog() {
     return (
         <div className="createBlog">
             <div className="createBlog__header">
+                <h2>Create blog</h2>
+                <ClearRoundedIcon />
                 
             </div >
             <div className="createBlog__info">
+                <AccountCircleIcon />
+                <div className="createBlog__userInfo">
+                    <h4>Name</h4>
+                    <button></button>
+                </div>
                 
             </div>
             <div className="createBlog__input">
-                
+                <form>
+                    <textarea placeholder="Write your blog here..."></textarea>
+                    <input placeholder="Add image/gif URL" />
+                </form>
+
             </div>
             <div className="createBlog__icons">
-                
+            <FontDownloadRoundedIcon/ >
             </div>
             <div className="createBlog__button">
 
